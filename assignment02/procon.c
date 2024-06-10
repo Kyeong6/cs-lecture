92bee6973426e6465445e59e22613fea852d0816
Kyeong6 <wwnss1411@gmail.com>
2024-06-11 01:28:02 +0900

    feat: 생산자-소비자로 구성된 응용프로그램 만들기

--- assignment02/procon.c
+++ assignment02/procon.c
@@ -13,8 +13,8 @@ int myread();
 pthread_mutex_t critical_section; // POSIX mutex
 sem_t semWrite, semRead; // POSIX semaphore
 int queue[N_COUNTER]; // shared buffer
-int wptr; // write pointer for queue[]
-int rptr; // read pointer for queue[]
+int wptr = 0; // write pointer for queue[]
+int rptr = 0; // read pointer for queue[]
 
 // producer thread function
 void* producer(void* arg) { 
@@ -44,13 +44,40 @@ void* consumer(void* arg) {
 
 // write n into the shared memory
 void mywrite(int n) { 
-  /* [Write here] */
+  // wait for empty space
+  sem_wait(&semWrite); 
+  
+  // enter critical section
+  pthread_mutex_lock(&critical_section);
+
+  queue[wptr] = n;
+  wptr = (wptr + 1) % N_COUNTER;
+
+  // leave critical section
+  pthread_mutex_unlock(&critical_section); 
+
+  // signal that there is a new item
+  sem_post(&semRead);
+
 }
 
 // write a value from the shared memory
 int myread() { 
-  /* [Write here] */
-  
+  // wait for an available item
+  sem_wait(&semRead);
+
+  // enter critical section
+  pthread_mutex_lock(&critical_section);
+
+  int n = queue[rptr];
+  rptr = (rptr + 1) % N_COUNTER;
+
+  // leave critical section
+  pthread_mutex_unlock(&critical_section);
+
+  // signal that there is empty space
+  sem_post(&semWrite);
+
   return n;
 }
 
@@ -61,7 +88,8 @@ int main() {
   pthread_mutex_init(&critical_section, NULL); // init mutex
 
   // init semaphore
-  /* [Write here] */
+  sem_init(&semWrite, 0, N_COUNTER); // initial value is the size of the buffer
+  sem_init(&semRead, 0, 0); // initial value is 0
 
   // create the threads for the producer and consumer
   pthread_create(&t[0], NULL, producer, NULL); 
@@ -71,7 +99,8 @@ int main() {
     pthread_join(t[i],NULL); // wait for the threads
 
   //destroy the semaphores
-  /* [Write here] */
+  sem_destroy(&semWrite);
+  sem_destroy(&semRead);
 
   pthread_mutex_destroy(&critical_section); // destroy mutex 
   return 0;
